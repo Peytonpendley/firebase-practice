7c733229e760b66417e8574c257cf77149124eb3
Peyton <peytonpendley18@gmail.com>
2023-03-13 10:07:38 -0500

    update files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { auth } from "./firebase/init"
+
+function App() {
+
+  function register() {
+    console.log("register")
+  }
+
+  return (
+    <div className="App">
+      <button onClick={register}>Register</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnWlyCYudAMTstG-s7vVTcrdoH5vy3iy0",
+  authDomain: "fir-practice-7a61a.firebaseapp.com",
+  projectId: "fir-practice-7a61a",
+  storageBucket: "fir-practice-7a61a.appspot.com",
+  messagingSenderId: "885411945482",
+  appId: "1:885411945482:web:ea7b64683a852cd8f6d3f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
